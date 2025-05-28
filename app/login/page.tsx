d70f014d67d01e971237e8b47d4a2a8583284ff2
peterjames3 <kamauchief97@gmail.com>
2025-05-28 12:17:52 +0300

    first commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,19 +2,29 @@ import { GraduationCap, Loader2 } from "lucide-react";
 import LoginForm from "@/app/ui/login-form";
 import { Suspense } from "react";
 import { Metadata } from "next";
+//import { redirect } from "next/navigation";
+//import getServerSession from "next-auth"; // ✅ Correct import
+//import { authConfig } from "@/auth.config";
+
 export const metadata: Metadata = {
   title: "Login",
   description: "Log in to the Edusion Dashboard",
 };
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  // const session = await getServerSession(authConfig);
+
+  // if (session) {
+  //   redirect("/dashboard");
+  // }
+
   return (
     <main className="flex items-center justify-center h-screen bg-background">
-      <div className="relative mx-auto flex w-full max-w-[490px] flex-col space-y-2.5 md:-mt-32 bg-gray-100  rounded-lg">
+      <div className="relative mx-auto flex w-full max-w-[490px] flex-col space-y-2.5 md:-mt-32 bg-gray-100 rounded-lg">
         <div className="flex h-35 w-full items-end rounded-lg bg-primary p-3 md:h-45">
           <div className="w-64 text-white md:w-64">
-            <div className="flex gap-2 items-center  font-semibold">
-              <GraduationCap size={64} className="pr-2  " />{" "}
+            <div className="flex gap-2 items-center font-semibold">
+              <GraduationCap size={64} className="pr-2" />
               <span className="text-3xl">Edusion</span>
             </div>
           </div>
