26e46c8dc0a5f178227d2fbf8e6a0516e6d85b02
peterjames3 <kamauchief97@gmail.com>
2025-09-05 15:34:44 +0300

    Replace _PORT to _SECURE in my transporter functiion

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -97,7 +97,7 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
-      secure: process.env.EMAIL_PORT,
+      secure: process.env.EMAIL_SECURE,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
